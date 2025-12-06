bc3f0a82ee1db8dfb3d779c43ae36660d9b58dab
Godspower Eze <godspower.eth@gmail.com>
2025-12-06 16:06:44 +0300

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+const MEMORY_SIZE: usize = 1 << 16;
+
+enum REGISTER {
+    R0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    PC, /* program counter */
+    COND,
+    COUNT
+}
+
+enum InstructionSet
+{
+    BR, /* branch */
+    ADD,    /* add  */
+    LD,     /* load */
+    ST,     /* store */
+    JSR,    /* jump register */
+    AND,    /* bitwise and */
+    LDR,    /* load register */
+    STR,    /* store register */
+    RTI,    /* unused */
+    NOT,    /* bitwise not */
+    LDI,    /* load indirect */
+    STI,    /* store indirect */
+    JMP,    /* jump */
+    RES,    /* reserved (unused) */
+    LEA,    /* load effective address */
+    TRAP    /* execute trap */
+}
+
+enum ConditionFlags
+{
+    POS = 1 << 0, /* P */
+    ZRO = 1 << 1, /* Z */
+    NEG = 1 << 2, /* N */
+}
+
+const PC_START: u16 = 0x3000; /* default starting position for the program counter */
+
+fn main() {
+    let mut memory: [u16; MEMORY_SIZE] = [0; MEMORY_SIZE];
+    let mut registers: [u16; REGISTER::COUNT as usize] = [0; REGISTER::COUNT as usize];
+
+    /* since exactly one condition flag should be set at any given time, set the Z flag */
+    registers[REGISTER::COND as usize] = ConditionFlags::ZRO as u16;
+
+    /* set the PC to starting position */
+    registers[REGISTER::PC as usize] = PC_START;
+
+    let mut running = true;
+    while running {
+        let pc = registers[REGISTER::PC as usize];
+        let instruction = memory[pc as usize];
+        registers[REGISTER::PC as usize] = pc.wrapping_add(1);
+
+        let op = instruction >> 12;
+        match op {
+            x if x == InstructionSet::ADD as u16 => {
+                
+            }
+            _ => {  }
+
+        }
+    }
+}
